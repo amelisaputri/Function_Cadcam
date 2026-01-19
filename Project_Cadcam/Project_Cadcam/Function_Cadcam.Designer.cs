3545bf4b513aa931d92325f891e1b556f1979e63
CSTI-CADCAM <cadcam.amelisa@dskorea.com>
2026-01-19 16:33:37 +0700

    Data baru 19/01/2026

---
+++ Project_Cadcam/Project_Cadcam/Function_Cadcam.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace Project_Cadcam
+{
+    partial class Function_Cadcam
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            textBoxData1 = new TextBox();
+            label2 = new Label();
+            textStatus1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textData2 = new TextBox();
+            dtpPlan = new DateTimePicker();
+            dtpStart = new DateTimePicker();
+            dtpEnd = new DateTimePicker();
+            label7 = new Label();
+            textStatus2 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            dtpPlan2 = new DateTimePicker();
+            dtpStart2 = new DateTimePicker();
+            dtpEnd2 = new DateTimePicker();
+            textPart = new TextBox();
+            textSizeX = new TextBox();
+            textSizeY = new TextBox();
+            textSizeZ = new TextBox();
+            Btn_Submit = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.WhiteSmoke;
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 17.8008881F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20.75F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.5F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 23.375F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Controls.Add(textBoxData1, 1, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 2);
+            tableLayoutPanel1.Controls.Add(textStatus1, 1, 2);
+            tableLayoutPanel1.Controls.Add(label3, 0, 3);
+            tableLayoutPanel1.Controls.Add(label4, 0, 4);
+            tableLayoutPanel1.Controls.Add(label5, 0, 5);
+            tableLayoutPanel1.Controls.Add(label6, 0, 7);
+            tableLayoutPanel1.Controls.Add(textData2, 1, 7);
+            tableLayoutPanel1.Controls.Add(dtpPlan, 1, 3);
+            tableLayoutPanel1.Controls.Add(dtpStart, 1, 4);
+            tableLayoutPanel1.Controls.Add(dtpEnd, 1, 5);
+            tableLayoutPanel1.Controls.Add(label7, 0, 8);
+            tableLayoutPanel1.Controls.Add(textStatus2, 1, 8);
+            tableLayoutPanel1.Controls.Add(label8, 0, 9);
+            tableLayoutPanel1.Controls.Add(label9, 0, 10);
+            tableLayoutPanel1.Controls.Add(label10, 0, 11);
+            tableLayoutPanel1.Controls.Add(label11, 0, 12);
+            tableLayoutPanel1.Controls.Add(label12, 0, 13);
+            tableLayoutPanel1.Controls.Add(dtpPlan2, 1, 9);
+            tableLayoutPanel1.Controls.Add(dtpStart2, 1, 10);
+            tableLayoutPanel1.Controls.Add(dtpEnd2, 1, 11);
+            tableLayoutPanel1.Controls.Add(textPart, 1, 12);
+            tableLayoutPanel1.Controls.Add(textSizeX, 1, 13);
+            tableLayoutPanel1.Controls.Add(textSizeY, 2, 13);
+            tableLayoutPanel1.Controls.Add(textSizeZ, 3, 13);
+            tableLayoutPanel1.Controls.Add(Btn_Submit, 4, 14);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 16;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 4.26802349F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.821465F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.185307F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.893937F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.300415F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.91013241F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 4.471263F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.91013241F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.503655F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.300415F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 7.92632866F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.50263834F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.50263834F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.50263834F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.502638F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.498364F));
+            tableLayoutPanel1.Size = new Size(800, 515);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(84, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Data 1";
+            // 
+            // textBoxData1
+            // 
+            textBoxData1.Anchor = AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(textBoxData1, 3);
+            textBoxData1.Location = new Point(144, 24);
+            textBoxData1.Name = "textBoxData1";
+            textBoxData1.Size = new Size(465, 23);
+            textBoxData1.TabIndex = 1;
+            textBoxData1.KeyDown += textBoxData1_KeyDown;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(83, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 19);
+            label2.TabIndex = 2;
+            label2.Text = "Status";
+            // 
+            // textStatus1
+            // 
+            textStatus1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(textStatus1, 3);
+            textStatus1.Location = new Point(144, 54);
+            textStatus1.Name = "textStatus1";
+            textStatus1.Size = new Size(465, 23);
+            textStatus1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(98, 86);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 19);
+            label3.TabIndex = 4;
+            label3.Text = "Plan";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(93, 117);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 19);
+            label4.TabIndex = 6;
+            label4.Text = "Start";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(102, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 19);
+            label5.TabIndex = 8;
+            label5.Text = "End";
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Right;
+            label6.AutoSize = true;
+            label6.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(82, 209);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 19);
+            label6.TabIndex = 9;
+            label6.Text = "Data 2";
+            // 
+            // textData2
+            // 
+            textData2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(textData2, 3);
+            textData2.Location = new Point(144, 207);
+            textData2.Name = "textData2";
+            textData2.Size = new Size(465, 23);
+            textData2.TabIndex = 7;
+            textData2.KeyDown += textData2_KeyDown;
+            // 
+            // dtpPlan
+            // 
+            dtpPlan.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(dtpPlan, 3);
+            dtpPlan.Location = new Point(144, 84);
+            dtpPlan.Name = "dtpPlan";
+            dtpPlan.Size = new Size(465, 23);
+            dtpPlan.TabIndex = 11;
+            // 
+            // dtpStart
+            // 
+            tableLayoutPanel1.SetColumnSpan(dtpStart, 3);
+            dtpStart.Location = new Point(144, 114);
+            dtpStart.Name = "dtpStart";
+            dtpStart.Size = new Size(465, 23);
+            dtpStart.TabIndex = 12;
+            // 
+            // dtpEnd
+            // 
+            tableLayoutPanel1.SetColumnSpan(dtpEnd, 3);
+            dtpEnd.Location = new Point(144, 146);
+            dtpEnd.Name = "dtpEnd";
+            dtpEnd.Size = new Size(465, 23);
+            dtpEnd.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(83, 243);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 19);
+            label7.TabIndex = 14;
+            label7.Text = "Status";
+            // 
+            // textStatus2
+            // 
+            textStatus2.Anchor = AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(textStatus2, 3);
+            textStatus2.Location = new Point(144, 241);
+            textStatus2.Name = "textStatus2";
+            textStatus2.Size = new Size(465, 23);
+            textStatus2.TabIndex = 15;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Right;
+            label8.AutoSize = true;
+            label8.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(98, 275);
+            label8.Name = "label8";
+            label8.Size = new Size(40, 19);
+            label8.TabIndex = 16;
+            label8.Text = "Plan";
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.Right;
+            label9.AutoSize = true;
+            label9.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(93, 311);
+            label9.Name = "label9";
+            label9.Size = new Size(45, 19);
+            label9.TabIndex = 17;
+            label9.Text = "Start";
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.Right;
+            label10.AutoSize = true;
+            label10.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(102, 348);
+            label10.Name = "label10";
+            label10.Size = new Size(36, 19);
+            label10.TabIndex = 18;
+            label10.Text = "End";
+            // 
+            // label11
+            // 
+            label11.Anchor = AnchorStyles.Right;
+            label11.AutoSize = true;
+            label11.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(98, 381);
+            label11.Name = "label11";
+            label11.Size = new Size(40, 19);
+            label11.TabIndex = 19;
+            label11.Text = "Part";
+            // 
+            // label12
+            // 
+            label12.Anchor = AnchorStyles.Right;
+            label12.AutoSize = true;
+            label12.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(99, 414);
+            label12.Name = "label12";
+            label12.Size = new Size(39, 19);
+            label12.TabIndex = 20;
+            label12.Text = "Size";
+            // 
+            // dtpPlan2
+            // 
+            tableLayoutPanel1.SetColumnSpan(dtpPlan2, 3);
+            dtpPlan2.Location = new Point(144, 272);
+            dtpPlan2.Name = "dtpPlan2";
+            dtpPlan2.Size = new Size(465, 23);
+            dtpPlan2.TabIndex = 21;
+            // 
+            // dtpStart2
+            // 
+            tableLayoutPanel1.SetColumnSpan(dtpStart2, 3);
+            dtpStart2.Location = new Point(144, 304);
+            dtpStart2.Name = "dtpStart2";
+            dtpStart2.Size = new Size(465, 23);
+            dtpStart2.TabIndex = 22;
+            // 
+            // dtpEnd2
+            // 
+            tableLayoutPanel1.SetColumnSpan(dtpEnd2, 3);
+            dtpEnd2.Location = new Point(144, 344);
+            dtpEnd2.Name = "dtpEnd2";
+            dtpEnd2.Size = new Size(465, 23);
+            dtpEnd2.TabIndex = 23;
+            // 
+            // textPart
+            // 
+            textPart.Anchor = AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(textPart, 3);
+            textPart.Location = new Point(144, 379);
+            textPart.Name = "textPart";
+            textPart.Size = new Size(465, 23);
+            textPart.TabIndex = 24;
+            // 
+            // textSizeX
+            // 
+            textSizeX.Anchor = AnchorStyles.Left;
+            textSizeX.Location = new Point(144, 412);
+            textSizeX.Name = "textSizeX";
+            textSizeX.Size = new Size(159, 23);
+            textSizeX.TabIndex = 25;
+            textSizeX.Text = "X";
+            // 
+            // textSizeY
+            // 
+            textSizeY.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            textSizeY.Location = new Point(309, 412);
+            textSizeY.Name = "textSizeY";
+            textSizeY.Size = new Size(145, 23);
+            textSizeY.TabIndex = 26;
+            textSizeY.Text = "Y";
+            // 
+            // textSizeZ
+            // 
+            textSizeZ.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            textSizeZ.Location = new Point(460, 412);
+            textSizeZ.Name = "textSizeZ";
+            textSizeZ.Size = new Size(149, 23);
+            textSizeZ.TabIndex = 27;
+            textSizeZ.Text = "Z";
+            // 
+            // Btn_Submit
+            // 
+            Btn_Submit.Anchor = AnchorStyles.None;
+            Btn_Submit.Font = new Font("Pretendard", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Btn_Submit.Location = new Point(666, 444);
+            Btn_Submit.Name = "Btn_Submit";
+            Btn_Submit.Size = new Size(80, 25);
+            Btn_Submit.TabIndex = 28;
+            Btn_Submit.Text = "Submit";
+            Btn_Submit.UseVisualStyleBackColor = true;
+            Btn_Submit.Click += Btn_Submit_Click;
+            // 
+            // Function_Cadcam
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 515);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Function_Cadcam";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Function_Cadcam";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxData1;
+        private Label label3;
+        private Label label4;
+        private TextBox textData2;
+        private Label label5;
+        private Label label6;
+        private TextBox textStatus1;
+        private DateTimePicker dtpPlan;
+        private DateTimePicker dtpStart;
+        private DateTimePicker dtpEnd;
+        private Label label7;
+        private TextBox textStatus2;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private DateTimePicker dtpPlan2;
+        private DateTimePicker dtpStart2;
+        private DateTimePicker dtpEnd2;
+        private TextBox textPart;
+        private TextBox textSizeX;
+        private TextBox textSizeY;
+        private TextBox textSizeZ;
+        private Button Btn_Submit;
+    }
+}
---
+++ Project_Cadcam/Project_Cadcam/Function_Cadcam.cs
@@ -0,0 +1,196 @@
+﻿using System.Globalization;
+
+namespace Project_Cadcam
+{
+    public partial class Function_Cadcam : Form
+    {
+        public Function_Cadcam()
+        {
+            InitializeComponent();
+            SetDateFormat();
+        }
+
+        //Change Date
+        private void SetDateFormat()
+        {
+            dtpPlan.Format = DateTimePickerFormat.Custom;
+            dtpPlan.CustomFormat = "yyyy-MM-dd";
+
+            dtpPlan2.Format = DateTimePickerFormat.Custom;
+            dtpPlan2.CustomFormat = "yyyy-MM-dd";
+
+            dtpStart.Format = DateTimePickerFormat.Custom;
+            dtpStart.CustomFormat = "yyyy-MM-dd";
+
+            dtpStart2.Format = DateTimePickerFormat.Custom;
+            dtpStart2.CustomFormat = "yyyy-MM-dd";
+
+            dtpEnd.Format = DateTimePickerFormat.Custom;
+            dtpEnd.CustomFormat = "yyyy-MM-dd";
+
+            dtpEnd2.Format = DateTimePickerFormat.Custom;
+            dtpEnd2.CustomFormat = "yyyy-MM-dd";
+        }
+
+        //-- Public function--//
+        // Parsing Data 1
+        private (bool valid, int status,
+        DateTime plan, DateTime start, DateTime end)
+        ParseData1(string input)
+        {
+            var p = input.Split('&');
+            if (p.Length != 4)
+                return default;
+
+            // STATUS = ANGKA SAJA
+            if (!int.TryParse(p[0], out int status))
+                return default;
+
+            if (!DateTime.TryParseExact(p[1], "yyyyMMdd", null,
+                DateTimeStyles.None, out var plan)) return default;
+
+            if (!DateTime.TryParseExact(p[2], "yyyyMMdd", null,
+                DateTimeStyles.None, out var start)) return default;
+
+            if (!DateTime.TryParseExact(p[3], "yyyyMMdd", null,
+                DateTimeStyles.None, out var end)) return default;
+
+            return (true, status, plan, start, end);
+        }
+
+
+        // Parsing Data 2
+        private (bool valid, int status,
+        DateTime plan, DateTime start, DateTime end,
+        string part, string sizeX, string sizeY, string sizeZ)
+        ParseData2(string input)
+        {
+            var p = input.Split('&');
+            if (p.Length != 6)
+                return default;
+
+            // ===== STATUS =====
+            if (!int.TryParse(p[0], out int status))
+                return default;
+
+            // ===== DATE =====
+            if (!DateTime.TryParseExact(p[1], "yyyyMMdd", null,
+                DateTimeStyles.None, out var plan)) return default;
+
+            if (!DateTime.TryParseExact(p[2], "yyyyMMdd", null,
+                DateTimeStyles.None, out var start)) return default;
+
+            if (!DateTime.TryParseExact(p[3], "yyyyMMdd", null,
+                DateTimeStyles.None, out var end)) return default;
+
+            // ===== PART =====
+            string part = p[4];
+
+            // ===== SIZE (100X200X50) =====
+            var size = p[5].Split('X');
+            if (size.Length != 3)
+                return default;
+
+            string sizeX = size[0];
+            string sizeY = size[1];
+            string sizeZ = size[2];
+
+            return (true, status, plan, start, end, part, sizeX, sizeY, sizeZ);
+        }
+        //-- Public function--//
+
+
+        // --- function Enter Universal---//
+        private void textBoxData1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+            e.SuppressKeyPress = true;
+
+            var d = ParseData1(textBoxData1.Text);
+            if (!d.valid)
+            {
+                MessageBox.Show("Format Data 1 salah");
+                return;
+            }
+
+            textStatus1.Text = d.status.ToString(); ;
+            dtpPlan.Value = d.plan;
+            dtpStart.Value = d.start;
+            dtpEnd.Value = d.end;
+        }
+
+        private void textData2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+            e.SuppressKeyPress = true;
+
+            var d = ParseData2(textData2.Text);
+            if (!d.valid)
+            {
+                MessageBox.Show("Format Data 2 salah");
+                return;
+            }
+
+            // ===== STATUS =====
+            textStatus2.Text = d.status.ToString();
+
+            // ===== DATE =====
+            dtpPlan2.Value = d.plan;
+            dtpStart2.Value = d.start;
+            dtpEnd2.Value = d.end;
+
+            // ===== PART =====
+            textPart.Text = d.part;
+
+            // ===== SIZE =====
+            textSizeX.Text = d.sizeX;
+            textSizeY.Text = d.sizeY;
+            textSizeZ.Text = d.sizeZ;
+        }
+        // --- function Enter Universal---//
+
+        //--- Function Edit ----//
+        private string BuildData1String()
+        {
+            int status = int.Parse(textStatus1.Text);
+
+            string plan = dtpPlan.Value.ToString("yyyyMMdd");
+            string start = dtpStart.Value.ToString("yyyyMMdd");
+            string end = dtpEnd.Value.ToString("yyyyMMdd");
+
+            return $"{status}&{plan}&{start}&{end}";
+        }
+
+        private string BuildData2String()
+        {
+            int status = int.Parse(textStatus2.Text);
+
+            string plan = dtpPlan2.Value.ToString("yyyyMMdd");
+            string start = dtpStart2.Value.ToString("yyyyMMdd");
+            string end = dtpEnd2.Value.ToString("yyyyMMdd");
+
+            string part = textPart.Text.Trim();
+
+            string size = $"{textSizeX.Text}X{textSizeY.Text}X{textSizeZ.Text}";
+
+            return $"{status}&{plan}&{start}&{end}&{part}&{size}";
+        }
+
+
+        // --- function Button Submit ---//
+        private void Btn_Submit_Click(object sender, EventArgs e)
+        {
+            // 🔁 FORM → STRING
+            string updatedData1 = BuildData1String();
+            string updatedData2 = BuildData2String();
+
+            // REFRESH textbox Data 1 & Data 2
+            textBoxData1.Text = updatedData1;
+            textData2.Text = updatedData2;
+
+            // (opsional) feedback
+            MessageBox.Show("Data berhasil diperbarui", "Submit");
+        }
+        // --- function Button Submit ---//
+    }
+}
