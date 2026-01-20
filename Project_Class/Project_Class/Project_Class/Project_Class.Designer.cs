d1fd7c432d2883e49663031d2431bc469a4ba102
Fikri <fikrim593@gmail.com>
2026-01-20 23:14:15 +0700

    Update 2026.01.20 add Label top

--- Project_Class/Project_Class/Project_Class/Project_Class.Designer.cs
+++ Project_Class/Project_Class/Project_Class/Project_Class.Designer.cs
@@ -29,51 +29,62 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
+            panel2 = new Panel();
             button1 = new Button();
             lb_judul = new Label();
-            flpRows = new FlowLayoutPanel();
-            dataGridView1 = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewTextBoxColumn();
-            Column6 = new DataGridViewTextBoxColumn();
-            Column7 = new DataGridViewTextBoxColumn();
-            Column8 = new DataGridViewTextBoxColumn();
-            Column9 = new DataGridViewTextBoxColumn();
-            Column10 = new DataGridViewTextBoxColumn();
-            Column11 = new DataGridViewTextBoxColumn();
-            Column12 = new DataGridViewTextBoxColumn();
+            panel3 = new Panel();
+            dgv1 = new DataGridView();
+            COL_SERIAL_NUM = new DataGridViewTextBoxColumn();
+            COL_PO_DATE = new DataGridViewTextBoxColumn();
+            COL_O_ETC = new DataGridViewTextBoxColumn();
+            COL_DELIVERY = new DataGridViewTextBoxColumn();
+            COL_R_ETC = new DataGridViewTextBoxColumn();
+            COL_STAGE = new DataGridViewTextBoxColumn();
+            COL_MOLD_CODE = new DataGridViewTextBoxColumn();
+            COL_SEASON = new DataGridViewTextBoxColumn();
+            COL_MODEL = new DataGridViewTextBoxColumn();
+            COL_PART = new DataGridViewTextBoxColumn();
+            COL_O_MOLD = new DataGridViewTextBoxColumn();
+            COL_PICTURE = new DataGridViewTextBoxColumn();
+            panelBandHeader = new Panel();
             tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv1).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.BackColor = Color.White;
-            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 88.24228F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.75772F));
-            tableLayoutPanel1.Controls.Add(button1, 1, 0);
-            tableLayoutPanel1.Controls.Add(lb_judul, 0, 0);
-            tableLayoutPanel1.Controls.Add(flpRows, 0, 2);
-            tableLayoutPanel1.Controls.Add(dataGridView1, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel3, 0, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 3;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 7.40740633F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 4.38957453F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 88.3401947F));
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.898491F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 94.10151F));
             tableLayoutPanel1.Size = new Size(1008, 729);
             tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(lb_judul);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1002, 37);
+            panel2.TabIndex = 5;
             // 
             // button1
             // 
             button1.Anchor = AnchorStyles.Right;
-            button1.Font = new Font("Pretendard", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(930, 15);
+            button1.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(909, 8);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
@@ -83,97 +94,104 @@
             // 
             // lb_judul
             // 
-            lb_judul.Anchor = AnchorStyles.Left;
-            lb_judul.AutoSize = true;
-            lb_judul.Font = new Font("Pretendard", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lb_judul.Location = new Point(3, 17);
+            lb_judul.Dock = DockStyle.Fill;
+            lb_judul.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lb_judul.Location = new Point(0, 0);
             lb_judul.Name = "lb_judul";
-            lb_judul.Size = new Size(122, 19);
+            lb_judul.Size = new Size(1002, 37);
             lb_judul.TabIndex = 2;
             lb_judul.Text = "Project Cadcam";
+            lb_judul.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // flpRows
+            // panel3
             // 
-            flpRows.BackColor = Color.WhiteSmoke;
-            tableLayoutPanel1.SetColumnSpan(flpRows, 2);
-            flpRows.Dock = DockStyle.Fill;
-            flpRows.FlowDirection = FlowDirection.TopDown;
-            flpRows.Location = new Point(3, 87);
-            flpRows.Name = "flpRows";
-            flpRows.Padding = new Padding(0, 0, 0, 5);
-            flpRows.Size = new Size(1002, 639);
-            flpRows.TabIndex = 0;
+            tableLayoutPanel1.SetColumnSpan(panel3, 2);
+            panel3.Controls.Add(dgv1);
+            panel3.Controls.Add(panelBandHeader);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(3, 46);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1002, 680);
+            panel3.TabIndex = 6;
             // 
-            // dataGridView1
+            // dgv1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8, Column9, Column10, Column11, Column12 });
-            tableLayoutPanel1.SetColumnSpan(dataGridView1, 2);
-            dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(3, 56);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(1002, 25);
-            dataGridView1.TabIndex = 3;
+            dgv1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv1.Columns.AddRange(new DataGridViewColumn[] { COL_SERIAL_NUM, COL_PO_DATE, COL_O_ETC, COL_DELIVERY, COL_R_ETC, COL_STAGE, COL_MOLD_CODE, COL_SEASON, COL_MODEL, COL_PART, COL_O_MOLD, COL_PICTURE });
+            dgv1.Location = new Point(0, 38);
+            dgv1.Name = "dgv1";
+            dgv1.Size = new Size(1002, 642);
+            dgv1.TabIndex = 3;
+            dgv1.ColumnWidthChanged += dgv1_ColumnWidthChanged;
+            dgv1.Scroll += dgv1_Scroll;
             // 
-            // Column1
+            // COL_SERIAL_NUM
             // 
-            Column1.HeaderText = "SERIAL NUM";
-            Column1.Name = "Column1";
+            COL_SERIAL_NUM.HeaderText = "SERIAL NUM";
+            COL_SERIAL_NUM.Name = "COL_SERIAL_NUM";
             // 
-            // Column2
+            // COL_PO_DATE
             // 
-            Column2.HeaderText = "PO-DATE";
-            Column2.Name = "Column2";
+            COL_PO_DATE.HeaderText = "PO-DATE";
+            COL_PO_DATE.Name = "COL_PO_DATE";
             // 
-            // Column3
+            // COL_O_ETC
             // 
-            Column3.HeaderText = "O-ETC";
-            Column3.Name = "Column3";
+            COL_O_ETC.HeaderText = "O-ETC";
+            COL_O_ETC.Name = "COL_O_ETC";
             // 
-            // Column4
+            // COL_DELIVERY
             // 
-            Column4.HeaderText = "DELIVERY";
-            Column4.Name = "Column4";
+            COL_DELIVERY.HeaderText = "DELIVERY";
+            COL_DELIVERY.Name = "COL_DELIVERY";
             // 
-            // Column5
+            // COL_R_ETC
             // 
-            Column5.HeaderText = "R-ETC";
-            Column5.Name = "Column5";
+            COL_R_ETC.HeaderText = "R-ETC";
+            COL_R_ETC.Name = "COL_R_ETC";
             // 
-            // Column6
+            // COL_STAGE
             // 
-            Column6.HeaderText = "STAGE";
-            Column6.Name = "Column6";
+            COL_STAGE.HeaderText = "STAGE";
+            COL_STAGE.Name = "COL_STAGE";
             // 
-            // Column7
+            // COL_MOLD_CODE
             // 
-            Column7.HeaderText = "MOLD-CODE";
-            Column7.Name = "Column7";
+            COL_MOLD_CODE.HeaderText = "MOLD-CODE";
+            COL_MOLD_CODE.Name = "COL_MOLD_CODE";
             // 
-            // Column8
+            // COL_SEASON
             // 
-            Column8.HeaderText = "SEASON";
-            Column8.Name = "Column8";
+            COL_SEASON.HeaderText = "SEASON";
+            COL_SEASON.Name = "COL_SEASON";
             // 
-            // Column9
+            // COL_MODEL
             // 
-            Column9.HeaderText = "MODEL";
-            Column9.Name = "Column9";
+            COL_MODEL.HeaderText = "MODEL";
+            COL_MODEL.Name = "COL_MODEL";
             // 
-            // Column10
+            // COL_PART
             // 
-            Column10.HeaderText = "PART";
-            Column10.Name = "Column10";
+            COL_PART.HeaderText = "PART";
+            COL_PART.Name = "COL_PART";
             // 
-            // Column11
+            // COL_O_MOLD
             // 
-            Column11.HeaderText = "O_MOLD-P";
-            Column11.Name = "Column11";
+            COL_O_MOLD.HeaderText = "O_MOLD-P";
+            COL_O_MOLD.Name = "COL_O_MOLD";
             // 
-            // Column12
+            // COL_PICTURE
             // 
-            Column12.HeaderText = "PICTURE";
-            Column12.Name = "Column12";
+            COL_PICTURE.HeaderText = "PICTURE";
+            COL_PICTURE.Name = "COL_PICTURE";
+            // 
+            // panelBandHeader
+            // 
+            panelBandHeader.Dock = DockStyle.Top;
+            panelBandHeader.Location = new Point(0, 0);
+            panelBandHeader.Name = "panelBandHeader";
+            panelBandHeader.Size = new Size(1002, 32);
+            panelBandHeader.TabIndex = 4;
             // 
             // Project_Class
             // 
@@ -183,30 +201,34 @@
             Controls.Add(tableLayoutPanel1);
             Name = "Project_Class";
             Text = "Project";
+            Load += Project_Class_Load;
             tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgv1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private FlowLayoutPanel flpRows;
         private Button button1;
         private Label lb_judul;
-        private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private DataGridViewTextBoxColumn Column3;
-        private DataGridViewTextBoxColumn Column4;
-        private DataGridViewTextBoxColumn Column5;
-        private DataGridViewTextBoxColumn Column6;
-        private DataGridViewTextBoxColumn Column7;
-        private DataGridViewTextBoxColumn Column8;
-        private DataGridViewTextBoxColumn Column9;
-        private DataGridViewTextBoxColumn Column10;
-        private DataGridViewTextBoxColumn Column11;
-        private DataGridViewTextBoxColumn Column12;
+        private DataGridView dgv1;
+        private DataGridViewTextBoxColumn COL_SERIAL_NUM;
+        private DataGridViewTextBoxColumn COL_PO_DATE;
+        private DataGridViewTextBoxColumn COL_O_ETC;
+        private DataGridViewTextBoxColumn COL_DELIVERY;
+        private DataGridViewTextBoxColumn COL_R_ETC;
+        private DataGridViewTextBoxColumn COL_STAGE;
+        private DataGridViewTextBoxColumn COL_MOLD_CODE;
+        private DataGridViewTextBoxColumn COL_SEASON;
+        private DataGridViewTextBoxColumn COL_MODEL;
+        private DataGridViewTextBoxColumn COL_PART;
+        private DataGridViewTextBoxColumn COL_O_MOLD;
+        private DataGridViewTextBoxColumn COL_PICTURE;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panelBandHeader;
     }
 }
--- Project_Class/Project_Class/Project_Class/Project_Class.cs
+++ Project_Class/Project_Class/Project_Class/Project_Class.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace Project_Class
 {
     public partial class Project_Class : Form
@@ -7,14 +9,100 @@ namespace Project_Class
             InitializeComponent();
         }
 
+        Label lblCadCam;
+
+        private void Project_Class_Load(object sender, EventArgs e)
+        {
+            FormatGrid(dgv1);
+            CreateLabel();
+            SyncBandWidth();
+        }
+
+        private void FormatGrid(DataGridView dgv)
+        {
+            if (dgv == null) return;
+
+            // Header center
+            dgv.EnableHeadersVisualStyles = false;
+            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.ColumnHeadersDefaultCellStyle.Font = new Font(dgv.Font, FontStyle.Bold);
+
+            // Auto size by HEADER (bukan isi)
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+
+            // === INI KUNCINYA ===
+            dgv.ScrollBars = ScrollBars.Both;
+
+            // Biar horizontal scroll muncul
+            dgv.Dock = DockStyle.Fill;   // atau NONE, asal jangan AutoSize
+
+            // Disable auto resize yang bikin kolom dipaksa muat layar
+            dgv.AutoSize = false;
+
+            // Cosmetic
+            //dgv.RowHeadersVisible = false;
+            //dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void CreateLabel()
+        {
+            lblCadCam = new Label()
+            {
+                Text = "CAD / CAM",
+                Height = 32,
+                TextAlign = ContentAlignment.MiddleCenter,
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.LightGray,
+                Font = new Font("Segoe UI", 9, FontStyle.Bold)
+            };
+
+            panelBandHeader.Controls.Add(lblCadCam);
+        }
+
+
+
+        private void SyncBandWidth()
+        {
+            if (lblCadCam == null) return;
+            if (dgv1.Columns.Count == 0) return;
+
+            int totalWidth = 0;
+            int startX = int.MaxValue;
+
+            foreach (DataGridViewColumn col in dgv1.Columns)
+            {
+                if (!col.Visible) continue;
+
+                totalWidth += col.Width;
+
+                Rectangle rect = dgv1.GetColumnDisplayRectangle(col.Index, true);
+                if (rect.X < startX)
+                    startX = rect.X;
+            }
+
+            if (startX == int.MaxValue)
+                startX = 0;
+
+            lblCadCam.Left = startX;
+            lblCadCam.Width = totalWidth;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            UC_ExcelRow row = new UC_ExcelRow();
 
-            // samakan lebar dengan FlowLayoutPanel
-            row.Width = flpRows.ClientSize.Width - 25;
+        }
 
-            flpRows.Controls.Add(row);
+        private void dgv1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (e.ScrollOrientation == ScrollOrientation.HorizontalScroll)
+            {
+                panelBandHeader.Left = -dgv1.HorizontalScrollingOffset;
+            }
+        }
+
+        private void dgv1_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
+        {
+            SyncBandWidth();
         }
     }
 }
